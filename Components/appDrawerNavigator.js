96c3a4ad2c4bdab1871d598a99bb70a26f5f1f50
Padmapriya29 <81081235+Padmapriya29@users.noreply.github.com>
2021-05-02 22:36:03 +0530

    Add files via upload

---
+++ Components/appDrawerNavigator.js
@@ -0,0 +1,11 @@
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { AppTabNavigator } from "./appTabNavigator";
+import CustomSideBarMenu from "./customSideBarMenu";
+
+export const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: { screen: AppTabNavigator },
+  },
+  { contentComponent: CustomSideBarMenu },
+  { initialRouteName: "Home" }
+);
